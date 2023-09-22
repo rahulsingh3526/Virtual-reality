f045128dee9952c0b0dbd474f03c88741b631132
Rahul_Singh <koderrahul@gmail.com>
2023-09-22 19:22:20 +0530

    trial

---
+++ pages/framer.ts
@@ -0,0 +1,196 @@
+export const headerVariants = {
+  hidden: {
+    opacity: 0,
+    y: -100,
+    transition: {
+      type: "spring",
+      stiffness: 300,
+      damping: 140,
+    },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+      delay: 0.8,
+    },
+  },
+};
+
+export const slideIn = (direction, type, delay, duration) => ({
+  hidden: {
+    x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+    y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+  },
+  show: {
+    x: 0,
+    y: 0,
+    transition: {
+      type,
+      delay,
+      duration,
+      ease: "easeOut",
+    },
+  },
+});
+
+export const staggerContainer = (staggerChildren, delayChildren) => ({
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren,
+      delayChildren,
+    },
+  },
+});
+
+export const textVariant = {
+  hidden: {
+    y: 50,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 1.25,
+      delay: 2.2,
+    },
+  },
+};
+
+export const textContainer = {
+  hidden: {
+    opacity: 0,
+  },
+  show: (i = 1) => ({
+    opacity: 1,
+    transition: { staggerChildren: 0.1, delayChildren: i * 0.15 },
+  }),
+};
+
+export const textVariant2 = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "tween",
+      ease: "easeIn",
+    },
+  },
+};
+
+export const fadeIn = (direction, type, delay, duration) => ({
+  hidden: {
+    x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+    y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+    opacity: 0,
+  },
+  show: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      type,
+      delay,
+      duration,
+      ease: "easeOut",
+    },
+  },
+});
+
+export const planetVariants = (direction) => ({
+  hidden: {
+    x: direction === "left" ? "-100%" : "100%",
+    rotate: 120,
+  },
+  show: {
+    x: 0,
+    rotate: 0,
+    transition: {
+      type: "spring",
+      duration: 1.8,
+      delay: 0.5,
+    },
+  },
+});
+
+export const zoomIn = (delay, duration) => ({
+  hidden: {
+    scale: 0,
+    opacity: 0,
+  },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "tween",
+      delay,
+      duration,
+      ease: "easeOut",
+    },
+  },
+});
+
+export const footerVariants = {
+  hidden: {
+    opacity: 0,
+    y: 50,
+    transition: {
+      type: "spring",
+      stiffness: 300,
+      damping: 140,
+    },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 80,
+      delay: 0.5,
+    },
+  },
+};
+
+// export const leftRight ={
+//   hidden : {
+//     opacity: 0,
+//     x: "100vw"
+//   },
+//   show : {
+//     opacity : 1,
+//     x: 0,
+//     transition: {
+//       type : "spring",
+//       delay: 2
+//     }
+//   }
+// }
+
+export const container2 = {
+  hidden: { opacity: 1, scale: 0 },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+export const item2 = {
+  hidden: { y: 50, opacity: 0 },
+  show: {
+    y: 0,
+    opacity: 1,
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { LuPhoneCall } from "react-icons/lu";
 import { FiMail } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { motion } from "framer-motion";
+import { container2, staggerContainer } from "./framer";
+import { TypingText } from "./Text";
 export default function Home() {
   return (
     // <div
@@ -16,25 +20,35 @@ export default function Home() {
           <nav className=" p-4 ">
             <div className="container mx-auto flex justify-between items-center">
               {/* Left side with image and name */}
-              <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-4 ">
                 <Image
                   src="/Frame.png" // Replace with the path to your image
                   alt="Logo"
-                  className="w-32 h-32"
+                  className="w-32 h-32 "
                   width={700}
                   height={700}
                 />
-                <Image
+                {/* <Image
                   src="/hydra.png" // Replace with the path to your image
                   alt="Logo"
                   className="w-12 h-8"
                   width={500}
                   height={500}
-                />
+                /> */}
+
+                <motion.p
+                  variants={staggerContainer}
+                  initial="hidden"
+                  whileInView="show"
+                  viewport={{ once: false, amount: 0.5 }}
+                  className="font-semibold text-3xl text-gradient w-40 hidden md:block "
+                >
+                  CHAIN CHRONICLES
+                </motion.p>
               </div>
 
               {/* Middle section with navigation links */}
-              <div className="flex space-x-12">
+              <div className="flex space-x-12 hidden xl:block ">
                 <Link
                   href="/about"
                   className="text-white hover:text-gray-200 text-xs"
@@ -45,13 +59,13 @@ export default function Home() {
                   href="/team"
                   className="text-white hover:text-gray-200 text-xs"
                 >
-                  SERVICES
+                  CREATE PROFILE
                 </Link>
                 <Link
                   href="/team"
                   className="text-white hover:text-gray-200 text-xs"
                 >
-                  TECHNOLOGIES
+                  hey{" "}
                 </Link>
                 <Link
                   href="/team"
@@ -63,32 +77,35 @@ export default function Home() {
               </div>
 
               {/* Right side with a button */}
-              <div className="space-x-8">
+              <span className=" sm:block xl:hidden ">
+                <GiHamburgerMenu
+                  style={{ height: "40px", width: "40px", color: "#8379B2" }}
+                />
+              </span>
+              <div className="space-x-8 hidden xl:block">
                 <button className="text-white border-2 border-white text-xs font-semibold py-2 px-4 rounded-full">
                   CONTACT US
                 </button>
                 <button className="bg-gradient-to-r from-[#8379B2] to-[#BFB6F6] text-xs font-semibold py-2 px-4 rounded-full">
-                  JOIN HYDRA
+                  LOGIN
                 </button>
               </div>
             </div>
           </nav>
           {/* hero section   */}
-          <div className="flex justify-between items-center p-4 ">
-            <div className="flex items-center lg:flex-col space-x-4 w-1/3">
-              <div className="">
-                <span className="font-semibold text-5xl text-gradient">
-                  Dive{" "}
+          <div className="flex flex-col-reverse lg:flex-row lg:justify-between items-center p-4 ">
+            <div className="flex items-center flex-col space-x-4 w-1/3">
+              <div className="space-y-1 text-2xl md:text-4xl">
+                <span className="font-semibold  text-gradient">
+                  CryptoCurrents{" "}
                 </span>
-                <span className="font-semibold text-white text-4xl ">
-                  Into The Depths Of
-                </span>
-                <span className="font-semibold text-5xl text-gradient ">
+                <span className="font-semibold text-white  ">Beyond</span>
+                <span className="font-semibold  text-gradient ">
                   {" "}
-                  Virtual Reality
+                  Blockchain Basics
                 </span>
               </div>
-              <p className="text-sm text-gray-500 mt-4">
+              <p className="text-sm text-gray-500 mt-4 hidden lg:block">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore nisl tincidunt
                 eget. Lectus mauris eros in vitae .
@@ -96,7 +113,7 @@ export default function Home() {
 
               <div className="flex justify-start space-x-8 mt-20">
                 <button className="rounded-full font font-semibold bg-gradient-to-r from-[#8379B2] to-[#BFB6F6] text-xs py-2 px-8">
-                  BUILD YOUR WORLD
+                  EXPLORE PROFILES
                 </button>{" "}
                 <Image
                   src="/arrow.png" // Replace with the path to your image
@@ -134,58 +151,13 @@ export default function Home() {
               />
             </div>
           </div>
-          <div className="flex bg-gradient-to-r from-[#211E2E] via-[#393355] to-[#211E2E] rounded-full justify-around p-5">
-            <div className="flex justify-around items-center">
-              <Image
-                src="/Page-1.png" // Replace with the path to your image
-                alt="Logo"
-                className="m-2"
-                width={40}
-                height={60}
-              />
-              <div className="space-y-1">
-                {" "}
-                <p className="text-xl font-bold text-white">Give us a visit</p>
-                <p className="text-xs text-white">
-                  Union St, Seattle, WA 98101, United States
-                </p>
-              </div>
-            </div>
-            <div className="flex justify-around items-center space-x-4 ">
-              <LuPhoneCall
-                style={{
-                  color: "#C2B2E1",
-                }}
-                className="w-12 h-16 pt-2"
-              />
-              <div className="space-y-1">
-                {" "}
-                <p className="text-xl font-bold text-white ">Give Us a Call</p>
-                <p className="text-xs text-white">(110) 1111-1010</p>
-              </div>
-            </div>
-            <div className="flex justify-around items-center space-x-4">
-              <FiMail
-                style={{
-                  color: "#C2B2E1",
-                }}
-                className="w-14 h-14 pt-2"
-              />
-              <div className="space-y-1">
-                {" "}
-                <p className="text-xl font-bold text-white pt-2">
-                  Send Us a Message
-                </p>
-                <p className="text-xs text-white">Contact@HydraVTech.com</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-row justify-between items-center p-4 m-4 ">
-            <div className=" space-y-2 w-[45%]">
+
+          <div className=" flex justify-between items-center p-4 m-4 ">
+            <div className=" space-y-2 w-[45%] hidden lg:block">
               {" "}
               <p className="font-bold text-white text-4xl">INTRODUCTION</p>
               <div className="flex flex-row items-center space-x-10">
-                <p className="text-white text-3xl font-thin">TO HYDRA VR </p>
+                <p className="text-white text-3xl font-thin">TO US</p>
                 <Image
                   src="/arrow.png" // Replace with the path to your image
                   alt="Logo"
@@ -195,7 +167,7 @@ export default function Home() {
                 />
               </div>
             </div>
-            <p className=" text-white w-[45%]">
+            <p className=" text-white w-[45%]  hidden lg:block">
               Vitae sapien pellentesque habitant morbi tristique senectus et
               netus et. Feugiat nibh sed pulvinar proin gravida hendrerit
               lectus. Mi sit amet mauris commodo quis imperdiet massa tincidunt
@@ -215,11 +187,19 @@ export default function Home() {
               />
             </div>{" "}
             <div className="w-[45%] space-y-5">
-              <p className="font-bold text-white text-4xl">ABOUT</p>
-              <p className="text-white text-3xl font-thin ">
-                TO HYDRA VR{" "}
-              </p>{" "}
-              <p className=" text-white">
+              <p className="font-bold text-white text-2xl lg:text-4xl ">
+                ABOUT
+              </p>
+              <motion.p
+                variants={container2}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: false, amount: 0.5 }}
+                className="text-white text-3xl font-thin "
+              >
+                <TypingText title="CHAIN CHRONICLES" textStyles="text-center" />
+              </motion.p>{" "}
+              <p className=" text-white text-xs lg:text-base">
                 Eget mi proin sed libero enim sed faucibus turpis. Nisl rhoncus
                 mattis rhoncus urna neque viverra justo. Vivamus at augue eget
                 arcu dictum. Ultrices gravida dictum fusce ut placerat orci.
@@ -232,18 +212,20 @@ export default function Home() {
                 nisl suscipit adipiscing bibendum. Leo a diam sollicitudi n
                 tempor.
               </p>
-              <button className="bg-gradient-to-r from-[#8379B2] to-[#BFB6F6] text-xs font-semibold py-2 px-4 rounded-full">
+              <button className="bg-gradient-to-r from-[#8379B2] to-[#BFB6F6] text-xs font-semibold py-2 px-4 rounded-full hidden">
                 Let s get in touch
               </button>
             </div>
           </div>
           {/* below the second image  */}
           <div className="flex flex-row justify-between items-center p-4 m-4 ">
-            <div className=" space-y-2 w-[45%]">
+            <div className=" space-y-2 w-[45%] hidden lg:block">
               {" "}
-              <p className="font-bold text-white text-4xl">WHY BUILD </p>
+              <p className="font-bold text-white text-4xl">WHY BLOG </p>
               <div className="flex flex-row items-center space-x-10">
-                <p className="text-white text-3xl font-thin">WITH HYDRA </p>
+                <p className="text-white text-3xl font-thin">
+                  WITH CHAIN CHRONICLES{" "}
+                </p>
                 <Image
                   src="/arrow.png" // Replace with the path to your image
                   alt="Logo"
@@ -253,7 +235,7 @@ export default function Home() {
                 />
               </div>
             </div>
-            <p className=" text-white w-[45%]">
+            <p className=" text-white w-[45%] hidden lg:block">
               Vitae sapien pellentesque habitant morbi tristique senectus et
               netus et. Feugiat nibh sed pulvinar proin gravida hendrerit
               lectus. Mi sit amet mauris commodo quis imperdiet massa tincidunt
@@ -264,43 +246,40 @@ export default function Home() {
           </div>
 
           {/* cards components    */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            <div className=" p-2 overflow-hidden m-2">
-              <div>
-                <div className="relative z-10">
-                  <Image
-                    src="/Mask group (1).png" // Replace with the path to your image
-                    alt="Logo"
-                    className="-z-10  absolute m-8 border-8 rounded-full border-[#B0B0B0] border-opacity-30"
-                    width={190}
-                    height={190}
-                  />
-                  <Image
-                    src="/Rectangle 11.png" // Replace with the path to your image
-                    alt="Logo"
-                    className=" absolute m-8 "
-                    width={250}
-                    height={511}
-                  />
+          {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            <div className="bg-gradient-radial from-[#413C5D] via-[#312539] to-[#211E2D] rounded-lg shadow-lg p-2 overflow-hidden m-2">
+              <div className="relative z-10">
+                <Image
+                  src="/Mask group (1).png" // Replace with the path to your image
+                  alt="Logo"
+                  className="  border-4  rounded-full "
+                  width={190}
+                  height={190}
+                />
+              </div>
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl text-white mb-2">htyhg</div>
+                <div className="border-t-[1px] h-0 w-48 "></div>
+                <div className="text-gray-700 text-base">
+                  {" "}
+                  Viverra aliquet eget sit amet tellus. Ornare lectus sit amet
+                  est placerat in. Lectus magna fringilla urna porttitor rhoncus
+                  vitae.
                 </div>
               </div>
               <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">htyhg</div>
-                <div className="text-gray-700 text-base">dasfvc</div>
-              </div>
-              <div className="px-6 py-4">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                <button className="bg-gradient-to-r from-[#8379B2] to-[#BFB6F6]   font-bold py-2 px-4 rounded-full">
                   Learn More
                 </button>
               </div>
             </div>
 
-            <div className=" bg-[#281D2D] bg-gradient-radial relative rounded-lg shadow-lg overflow-hidden m-2">
+            <div className="  bg-gradient-radial from-[#413C5D] via-[#312539] to-[#211E2D] relative rounded-lg shadow-lg overflow-hidden m-2">
               <div className="">
                 <Image
                   src="/Mask group (2).png" // Replace with the path to your image
                   alt="Logo"
-                  className="m-8 p-4 border-8 rounded-full border-[#B0B0B0] border-opacity-50"
+                  className="m-8 blur-sm p-4 border-8 rounded-full border-[#B0B0B0] border-opacity-50"
                   layout="responsive"
                   width={200}
                   height={80}
@@ -356,9 +335,9 @@ export default function Home() {
                 </button>
               </div>
             </div>
-          </div>
+          </div> */}
 
-          <div className="flex flex-row justify-center align-middle relative">
+          {/* <div className="flex flex-row justify-center align-middle relative">
             {" "}
             <p className="font-bold text-white text-4xl absolute top-20">
               TECHNOLOGIES & HARDWARE
@@ -384,18 +363,65 @@ export default function Home() {
               width={200}
               height={80}
             />
+          </div> */}
+
+          <div className="flex flex-col md:flex-row bg-gradient-to-r from-[#211E2E] via-[#393355] to-[#211E2E] rounded-lg md:rounded-full justify-around p-5 mx-5 my-20">
+            <div className="flex justify-around items-center">
+              <Image
+                src="/Page-1.png" // Replace with the path to your image
+                alt="Logo"
+                className="m-2"
+                width={40}
+                height={60}
+              />
+              <div className="space-y-1">
+                {" "}
+                <p className="text-xl font-bold text-white">Give us a visit</p>
+                <p className="text-xs text-white">
+                  Union St, Seattle, WA 98101, United States
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-around items-center space-x-4 ">
+              <LuPhoneCall
+                style={{
+                  color: "#C2B2E1",
+                }}
+                className="w-12 h-16 pt-2"
+              />
+              <div className="space-y-1">
+                {" "}
+                <p className="text-xl font-bold text-white ">Give Us a Call</p>
+                <p className="text-xs text-white">(110) 1111-1010</p>
+              </div>
+            </div>
+            <div className="flex justify-around items-center space-x-4">
+              <FiMail
+                style={{
+                  color: "#C2B2E1",
+                }}
+                className="w-14 h-14 pt-2"
+              />
+              <div className="space-y-1">
+                {" "}
+                <p className="text-xl font-bold text-white pt-2">
+                  Send Us a Message
+                </p>
+                <p className="text-xs text-white">Contact@HydraVTech.com</p>
+              </div>
+            </div>
           </div>
 
           {/* footer  */}
-          <div className="flex flex-row justify-between">
+          <div className="flex flex-col md:flex-row justify-between">
             <Image
               src="/Frame.png" // Replace with the path to your image
               alt="Logo"
-              className="m-8"
+              className="m-8 hidden lg:block"
               width={120}
               height={120}
             />
-            <div className="h-40 w-1 bg-gradient-to-b from-[#3F3349] via-[#B2A2D3] to-[#3F3349]"></div>
+            <div className="h-40 w-1 bg-gradient-to-b from-[#3F3349] via-[#B2A2D3] to-[#3F3349] hidden lg:block"></div>
             <div>
               <p className="text-white ">ABOUT</p>
               <p className="text-white ">SERVICES </p>
@@ -403,15 +429,15 @@ export default function Home() {
               <p className="text-white ">HOW TO</p>
               <p className="text-white ">JOIN HYDRA</p>
             </div>
-            <div className="h-40 w-1 bg-gradient-to-b from-[#3F3349] via-[#B2A2D3] to-[#3F3349]"></div>
-            <div>
+            <div className="h-40 w-1 bg-gradient-to-b from-[#3F3349] via-[#B2A2D3] to-[#3F3349] hidden md:block"></div>
+            <div className="hidden lg:block">
               <p className="text-white">ABOUT</p>
               <p className="text-white">SERVICES </p>
               <p className="text-white">TECHNOLOGIES</p>
               <p className="text-white">HOW TO</p>
               <p className="text-white">JOIN HYDRA</p>
             </div>
-            <div className="h-40 w-1 bg-gradient-to-b from-[#3F3349] via-[#B2A2D3] to-[#3F3349]"></div>
+            <div className="h-40 w-1 bg-gradient-to-b from-[#3F3349] via-[#B2A2D3] to-[#3F3349] hidden lg:block"></div>
             <div>
               <p className="text-white">SOCIALISE WITH HYDRA</p>
               <Image
@@ -426,6 +452,12 @@ export default function Home() {
               </button>
             </div>
           </div>
+          <div className=" h-[1px] flex justify-center m-12  bg-gradient-to-r from-[#3F3349] via-[#B2A2D3] to-[#3F3349]"></div>
+
+          <p className="flex justify-center text-xs md:text-sm text-white ">
+            2023 © HYDRA LANDING PAGE - BY ZINE. E. FALOUTI - ALL RIGHTS
+            RESERVED{" "}
+          </p>
         </div>
       </div>
     </>
